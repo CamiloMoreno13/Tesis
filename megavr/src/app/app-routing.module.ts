bd632d3f481fac4f99044591d10b71d98b58615a
Héctor Rodríguez <h_rodriguez@javeriana.edu.co>
2021-06-21 20:06:16 -0500

    Aframe básico funciona

--- megavr/src/app/app-routing.module.ts
+++ megavr/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SpaceComponent } from './space/space.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'space',component:SpaceComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- megavr/src/app/app.module.ts
+++ megavr/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core'; 
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SpaceComponent } from './space/space.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpaceComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
