278404180ac8cf66f6441cad545546360673894d
Héctor Rodríguez <h_rodriguez@javeriana.edu.co>
2021-06-30 16:26:17 -0500

    New branch
    Property 'components' does not exist on type 'Element' - comentado

--- megavr/src/app/space/space.component.ts
+++ megavr/src/app/space/space.component.ts
@@ -1,8 +1,14 @@
 import { createElementCssSelector } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+
 //import * as THREE from 'three';
 //import { Scene } from 'three';
-
+/*export interface Post {
+  titulo: string;
+  contenido: string;
+  estracto: string;
+  url_image: string;
+}*/
 @Component({
   selector: 'app-space',
   templateUrl: './space.component.html',
@@ -43,12 +49,54 @@ export class SpaceComponent implements OnInit {
 
       animate();
       */
-     /*
+
+        
     let caja = document.getElementById('caja');
-    caja?.setAttribute('color', 'red');
+    caja?.setAttribute('geometry', 'primitive: box');
+    caja?.setAttribute('material','src :../assets/arte.jpg')
     caja?.setAttribute('position', '1 2 -5');
     caja?.setAttribute('rotation', '0 45 45');
-      */
+    caja?.setAttribute('sound','src:../assets/bichota.mp3 ; on: click')
+    
+    /*var audioEl = document.querySelector("a-scene");
+    console.log(audioEl);
+    audioEl.components*/
+    
+    
+    
+ 
+    let cono = document.getElementById('cono');
+    
+    cono?.setAttribute('geometry', 'primitive:cone');
+    cono?.setAttribute('material', 'color:blue');
+    cono?.setAttribute('position', '-4 -1.34385 -7.16797');
+  
+    let ball = document.getElementById('bola');
+      ball?.setAttribute('geometry', 'primitive:sphere');
+    ball?.setAttribute('material', 'color:yellow');
+    ball?.setAttribute('position', '-4.12706 0.10737 -7.34661');
+      console.log(ball)
+
+      let fondo = document.getElementById('fondo')
+      fondo?.setAttribute('src','https://i.imgur.com/jL7ZMrY.jpeg')
+
+    let cono2=document.createElement('a-entity');
+    cono2.setAttribute('id','caji');
+    cono2?.setAttribute('geometry', 'primitive:box');
+    cono2?.setAttribute('material', 'color:green');
+    cono2?.setAttribute('position', '-7 2 -5');
+
+      let escena = document.querySelector('a-scene');
+      escena?.appendChild(cono2);
+
+
+
+
+  
+
+  
+
+
   }
 
 }
