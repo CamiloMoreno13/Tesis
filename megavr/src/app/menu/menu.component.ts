01ec39fef73a3c91ffa776a3640a144ff52b7aaa
Héctor Rodríguez <h_rodriguez@javeriana.edu.co>
2021-07-20 14:53:23 -0500

    Menu básico

--- megavr/src/app/menu/menu.component.ts
+++ megavr/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { style } from '@angular/animations';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import * as $ from 'jquery';
 
 @Component({
@@ -8,38 +9,52 @@ import * as $ from 'jquery';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+
+  @ViewChild('pls') pls!: ElementRef;
+  @ViewChild('item1') item1!: ElementRef;
+  @ViewChild('item2') item2!: ElementRef;
+  @ViewChild('item3') item3!: ElementRef;
+  @ViewChild('item4') item4!: ElementRef;
+  @ViewChild('item5') item5!: ElementRef;
+
+  
+
+  @ViewChild('menu-items') menu!: ElementRef;
+
+  private bola : boolean = false;
+  constructor(private renderer2: Renderer2) { }
 
   ngOnInit(): void {
-    var nbOptions:number = 8;
-    var angleStart:number = -360;
 
-    function toggleOptions(s: any) {
-      $(s).toggleClass('open');
-      var li = $(s).find('li');
-      var deg = $(s).hasClass('half') ? 180/(li.length-1) : 360/li.length;
-      for(var i=0; i<li.length; i++) {
-        var d = $(s).hasClass('half') ? (i*deg)-90 : i*deg;
-        $(s).hasClass('open') ? rotate(li[i],d) : rotate(li[i],-360);
-      }
-    }
-  
-     // jquery rotate animation
-    function rotate(li: HTMLLIElement,d: number) {
-      $({d:-360}).animate({d:d}, {
-        step: function(now) {
-          $(li)
-          .css({ transform: 'rotate('+now+'deg)' })
-          .find('label')
-          .css({ transform: 'rotate('+(-now)+'deg)' });
-        }, duration: 0
-      });
-    }
-
-    const objeto:any = $('.selector');
-    $('.selector button').click(function(){
-      toggleOptions(objeto);
-    });
   }
 
+
+
+  pop() {
+    console.log("perro");
+    var i = 0;
+    var b1 = this.item1.nativeElement;
+    var b2 = this.item2.nativeElement;
+    var b3 = this.item3.nativeElement;
+    var b4 = this.item4.nativeElement;
+    var b5 = this.item5.nativeElement;
+    
+
+    if (!this.bola) {
+      this.renderer2.setStyle(b1, 'transform', 'translateX(-150px)');
+      this.renderer2.setStyle(b2,'transform','translate(-100px,-100px)');
+      this.renderer2.setStyle(b3,'transform','translateY(-150px)');
+      this.renderer2.setStyle(b4,'transform','translate(100px,-100px)');
+      this.renderer2.setStyle(b5,'transform','translateX(150px)');
+      this.bola=true;
+    }
+    else{
+      this.renderer2.setStyle(b1, 'transform', 'translate(0px)');
+      this.renderer2.setStyle(b2,'transform', 'translate(0px)');
+      this.renderer2.setStyle(b3,'transform', 'translate(0px)');
+      this.renderer2.setStyle(b4,'transform', 'translate(0px)');
+      this.renderer2.setStyle(b5,'transform', 'translate(0px)');
+      this.bola=false;
+    }
+  }
 }
