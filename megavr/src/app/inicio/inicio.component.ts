a7cb6d121119d9fa8ef3359c1ea4df0f4ae69d89
Camilo Moreno <camilomoreno@javeriana.edu.co>
2021-07-20 11:04:00 -0700

    boarding 100%m

--- megavr/src/app/inicio/inicio.component.ts
+++ megavr/src/app/inicio/inicio.component.ts
@@ -8,68 +8,93 @@ import { Router } from '@angular/router';
 })
 export class InicioComponent implements OnInit {
 
-  public primera:boolean = true;
-  public segunda:boolean = false;
-  public tercera:boolean = false;
-  public cuarta:boolean = false;
-  public quinta:boolean = false;
+  public primera: boolean = true;
+  public segunda: boolean = false;
+  public tercera: boolean = false;
+  public cuarta: boolean = false;
+  public quinta: boolean = false;
   public texto: string = "";
-  constructor(private router:Router) { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.texto = "QUE PASA ALDEMAR"
   }
-  
-  fprimera(){
+
+  fprimera() {
     console.log("que pasa");
-    if(this.primera){
+    if (this.primera) {
       this.primera = false;
       this.segunda = true;
-    } else{
+    } else {
       this.primera = true;
       this.segunda = false;
     }
   }
-  next2(){
-    if(this.segunda){
+  next2() {
+    if (this.segunda) {
       this.segunda = false;
       this.tercera = true;
-    } 
+    }
   }
-  next3(){
-    if(this.tercera){
+  next3() {
+    if (this.tercera) {
       this.tercera = false;
       this.cuarta = true;
     }
   }
-  next4(){
-    if(this.cuarta){
+  next4() {
+    if (this.cuarta) {
       this.cuarta = false;
-      this.quinta = true; 
+      this.quinta = true;
     }
   }
-  back3(){
-    if(this.tercera){
+  back3() {
+    if (this.tercera) {
       this.tercera = false;
       this.segunda = true;
     }
   }
 
-  back4(){
-    if(this.cuarta){
+  back4() {
+    if (this.cuarta) {
       this.cuarta = false;
       this.tercera = true;
     }
   }
 
-  back5(){
-    if(this.quinta){
+  back5() {
+    if (this.quinta) {
       this.quinta = false;
       this.cuarta = true;
     }
   }
-  space(){
+  space() {
     this.router.navigate(['/space'])
   }
 
+  indi1() {
+    this.segunda = true;
+    this.tercera = false;
+    this.cuarta = false;
+    this.quinta = false;
+
+  }
+  indi2() {
+    this.segunda = false;
+    this.tercera = true;
+    this.cuarta = false;
+    this.quinta = false;
+  }
+  indi3() {
+    this.segunda = false;
+    this.tercera = false;
+    this.cuarta = true;
+    this.quinta = false;
+  }
+  indi4(){
+    this.segunda = false;
+    this.tercera = false;
+    this.cuarta = false;
+    this.quinta = true;
+  }
 }
