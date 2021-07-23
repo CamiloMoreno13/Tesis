d32ac841aac9cfc10cd107eab9c5e6ca0e6a5bf8
Héctor Rodríguez <h_rodriguez@javeriana.edu.co>
2021-07-23 17:13:01 -0500

    Hosting Firebase funciona
    Hosting firebase base

--- megavr/src/environments/environment.prod.ts
+++ megavr/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebaseConfig : {
+  apiKey: "AIzaSyCHR1kCr7uRTA_h5ESTaiKenk7Jo18yuuU",
+    authDomain: "tesisvr-21.firebaseapp.com",
+    databaseURL: "https://tesisvr-21-default-rtdb.firebaseio.com",
+    projectId: "tesisvr-21",
+    storageBucket: "tesisvr-21.appspot.com",
+    messagingSenderId: "979640409708",
+    appId: "1:979640409708:web:ef6f3bc2734ca0fd3fc249",
+    measurementId: "G-ZD874PGVQY"
+
+}}
