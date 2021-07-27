8237dc606792b21ce315b51c1e0c8ff307174bd9
Héctor Rodríguez <h_rodriguez@javeriana.edu.co>
2021-07-27 12:10:21 -0500

    Botón mute/unmute

--- megavr/src/app/perfiles/perfiles.component.ts
+++ megavr/src/app/perfiles/perfiles.component.ts
@@ -1,5 +1,5 @@
 import { Route } from '@angular/compiler/src/core';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit,Renderer2, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -8,13 +8,26 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./perfiles.component.css']
 })
 export class PerfilesComponent implements OnInit {
+  @ViewChild('videopls') videopls!: ElementRef;
+  @ViewChild('videoi') videoi!: ElementRef;
+  //volume : boolean = true;
   public parametro: string | null = "";
-  constructor(private routes: ActivatedRoute) { }
+  constructor(private renderer2: Renderer2,private routes: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.parametro = this.routes.snapshot.paramMap.get('id');
     console.log(this.parametro);
   }
 
-
+  mute(){
+    var b1 = this.videoi.nativeElement;
+    if(this.videopls.nativeElement.muted){
+      
+      this.videopls.nativeElement.muted = false;
+    }
+    else{
+      this.videopls.nativeElement.muted=true;
+      this.videoi.nativeElement.class.setAttribute('class','fa fa-volume-mute');
+    }
+  }
 }
