20cfa078094db1c4e13e925d16b7a95cde73b0a5
Héctor Rodríguez <h_rodriguez@javeriana.edu.co>
2021-08-03 20:33:26 -0500

    Cursor + objeto 3d por ts

--- megavr/src/app/space/space.component.ts
+++ megavr/src/app/space/space.component.ts
@@ -15,6 +15,15 @@ export class SpaceComponent implements OnInit {
   ngOnInit(): void {
     
     let caja = document.getElementById('caja');
+    let pc = document.getElementById('pc');
+
+    pc?.setAttribute('gltf-model','../../assets/laptop.glb');
+    pc?.setAttribute('scale','3 3 3');
+    pc?.setAttribute('position','-4.4 -1.3 -7.3'); 
+    pc?.setAttribute('rotation',' 0 90 0'); 
+   
+
+
     caja?.setAttribute('geometry', 'primitive: box');
     caja?.setAttribute('material', 'color:red');
     caja?.setAttribute('position', '1 2 -5');
