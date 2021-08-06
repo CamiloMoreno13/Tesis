accb7c71a95516d2d8704e0e00e3debe7db385b8
Camilo Moreno <camilomoreno@javeriana.edu.co>
2021-08-05 23:28:48 -0500

    OnBoarding 100% responsive

--- megavr/src/app/inicio/banner.ts
+++ megavr/src/app/inicio/banner.ts
@@ -1,21 +1,16 @@
 export class banner{
     public title: string;
     public subtitle : string;
-    public imageT: string; 
-    public imageS: string; 
 
     constructor(){
         this.title = "";
         this.subtitle = "";
-        this.imageT = ""; 
-        this.imageS = "";
     }
 
     createBanner (title: string, sub: string): banner{
         let nuevo = new banner();
         nuevo.title = title;
         nuevo.subtitle = sub; 
-        nuevo.imageT = "../../assets/back.svg";
         return nuevo;
     }
 }
--- megavr/src/app/inicio/inicio.component.ts
+++ megavr/src/app/inicio/inicio.component.ts
@@ -1,6 +1,7 @@
 import { compileNgModule } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { transpileModule } from 'typescript';
 import { banner } from './banner';
 
 @Component({
@@ -16,43 +17,57 @@ export class InicioComponent implements OnInit {
   public title: string = "";
   public sub: string = "";
   public indice: number = 0;
-  public tam : number = 0;
-  public imagen : string = "";
-  public indicador : boolean[] = [false,false,false,false,false];
+  public tam: number = 0;
+  public btitle: boolean = false;
+  public bsub: boolean = false;
+  public indicador: boolean[] = [false, false, false, false, false];
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.indicadores(0);
     this.llenar();
-    this.inicio();   
+    this.inicio();
   }
-  llenar(){
+  llenar() {
     this.informacion.push(new banner().createBanner("Bienvenida", "Hola te damos la bienvenida a la mega virtual"));
-    this.informacion.push(new banner().createBanner("adios", "Para navegar usa tu celular de la forma horizontal y conoce el contenido tocando sobre las diferentes iconos"));
+    this.informacion.push(new banner().createBanner("", "Para navegar usa tu celular de la forma horizontal y conoce el contenido tocando sobre las diferentes iconos"));
     this.informacion.push(new banner().createBanner("Sabes donde se sienta cada DJ", "Vas a poder interactuar con cada uno de los Dj's de la mega y saber las noticias del entretenimiento"));
-    this.informacion.push(new banner().createBanner("Sabes que?", "La mega se oye más del 50% de Colombia '<br>' La Megaesta al aire desde 1993"));
+    this.informacion.push(new banner().createBanner("Sabes que?", "La mega se oye más del 50% de Colombia La Megaesta al aire desde 1993"));
     this.informacion.push(new banner().createBanner("Compártelo con tus amigos", ""));
   }
-  inicio(){
+  inicio() {
     this.title = this.informacion[this.indice].title;
     this.sub = this.informacion[this.indice].subtitle;
-    this.tam = this.informacion.length - 1 ;
+    this.tam = this.informacion.length - 1;
+    this.bback = false; 
+    this.bnext = true; 
+    this.bsub = false; 
+    this.btitle = false; 
   }
   principal() {
     this.open = false;
-    this.bback = false;
   }
   next() {
     this.indice += 1;
     this.indicadores(this.indice);
     this.bback = true;
-    if(this.indice < this.tam){
-      this.title =  this.informacion[this.indice].title;
+    if (this.indice == 1) {
+      this.btitle = true;
+    } else {
+      this.btitle = false;
+    }
+    if (this.indice == this.tam) {
+      this.bsub = true;
+    } else {
+      this.bsub = false;
+    }
+    if (this.indice < this.tam) {
+      this.title = this.informacion[this.indice].title;
       this.sub = this.informacion[this.indice].subtitle;
-    } else{
+    } else {
       this.bnext = false;
-      this.title =  this.informacion[this.indice].title;
+      this.title = this.informacion[this.indice].title;
       this.sub = this.informacion[this.indice].subtitle;
     }
   }
@@ -60,22 +75,74 @@ export class InicioComponent implements OnInit {
     this.indice -= 1;
     this.indicadores(this.indice);
     this.bnext = true;
-    if(this.indice > 0){
-      this.title =  this.informacion[this.indice].title;
+    if (this.indice == 1) {
+      this.btitle = true;
+    } else {
+      this.btitle = false;
+    }
+
+    if (this.indice == this.tam) {
+      this.bsub = true;
+    } else {
+      this.bsub = false;
+    }
+    if (this.indice > 0) {
+      this.title = this.informacion[this.indice].title;
       this.sub = this.informacion[this.indice].subtitle;
-    } else{
+    } else {
       this.bback = false;
-      this.title =  this.informacion[this.indice].title;
+      this.title = this.informacion[this.indice].title;
       this.sub = this.informacion[this.indice].subtitle;
     }
   }
-  indicadores(index: number){
-    for(let i = 0; i < this.indicador.length; i++){
-      if(i == index){
+  indicadores(index: number) {
+    for (let i = 0; i < this.indicador.length; i++) {
+      if (i == index) {
         this.indicador[i] = true;
-      }else{
+      } else {
         this.indicador[i] = false;
       }
     }
+  }
+  saltar(index: number) {
+    this.indice = index;
+    this.indicadores(index);
+    if (index == 0) {
+      console.log("entro")
+      this.bback = false;
+      this.bnext = true;
+    }
+    if (index == 1) {
+      this.bnext = true;
+      this.bback = true;
+      this.btitle = true;
+    } else {
+      this.btitle = false;
+    }
+    if (index != 1 && index != 0 && index != this.tam) {
+      this.bback = true;
+      this.bback = true;
+      this.btitle = false;
+      this.bsub = false;
+    }
+    if (index == this.tam) {
+      this.bnext = false;
+      this.bsub = true;
+      this.bback = true;
+    } else {
+      this.bsub = false;
+    }
+    this.title = this.informacion[index].title;
+    this.sub = this.informacion[index].subtitle;
+  }
+  space(){
+    this.router.navigate(['/space']); 
+  }
+
+  exit(){
+    this.open = true;
+    this.indice = 0;  
+    this.indicadores(this.indice);
+    this.inicio();
   }
 }
