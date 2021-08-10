a3a964b3da403f6cdc6e5a66c9e9d496ced6985d
Héctor Rodríguez <h_rodriguez@javeriana.edu.co>
2021-08-09 22:41:52 -0500

    Menu en carga de escena

--- megavr/src/app/space/space.component.ts
+++ megavr/src/app/space/space.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { AFrame } from 'aframe';
 
+
 @Component({
   selector: 'app-space',
   templateUrl: './space.component.html',
@@ -14,38 +15,44 @@ export class SpaceComponent implements OnInit {
   //@ViewChild('caroCab') caroCab!: HTMLElement;
 
   @ViewChild('caroCab') Hs!: ElementRef;
-  public mostrar: boolean = false; 
+  public mostrar: boolean = false;
 
-  constructor(private router : Router) { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
-    
+
     let caja = document.getElementById('caja');
     let pc = document.getElementById('pc');
 
     pc?.setAttribute('gltf-model','../../assets/Shared/Notebook.glb');
     pc?.setAttribute('scale','0.04 0.04 0.04');
     pc?.setAttribute('position','-19.2 -99.9 52.3'); 
-    pc?.setAttribute('rotation',' -0.42 86.8 -7.5'); 
+    pc?.setAttribute('rotation',' -0.42 86.8 -7.5');
 
     caja?.setAttribute('geometry', 'primitive: box');
     caja?.setAttribute('material', 'color:red');
     caja?.setAttribute('position', '1 2 -5');
     caja?.setAttribute('rotation', '0 45 45');
 
-    var entity= document.querySelector('#yellow') ;
-    if(entity != null){
-      console.log("enity" , entity);
+    var entity = document.querySelector('#yellow');
+    if (entity != null) {
+      console.log("enity", entity);
     }
-    
-    while(!this.mostrar){
-      if(document.querySelector("a-scene").hasLoaded){
-        this.mostrar = true; 
+    document.querySelector("a-scene").addEventListener("loaded",() =>{ this.mostrar=true;}) /* This is the key*/
+    /*let cont = 0;
+    setTimeout(() => {
+      console.log("magiaaaaa");
+      
+      let cons = document.querySelector("a-scene").hasLoaded;
+      console.log("cons es ",cons);
+      if (cons == true){
+        this.mostrar=true;
       }
-    }
+    }, 8000);*/
   }
 
-  prueba(){
+
+  prueba() {
     let valor = document.querySelector('#caja');
     console.log(valor);
     valor?.setAttribute('color', 'red');
@@ -57,7 +64,7 @@ export class SpaceComponent implements OnInit {
     this.caroCab.
   }*/
 
-  H(){
+  H() {
     this.Hs.nativeElement.material.components.src.stopVideo();
   }
 
@@ -65,14 +72,14 @@ export class SpaceComponent implements OnInit {
     console.log("Funciona enbebido")
   }
 
-  locutor(cadena:string){
-    this.router.navigate(['/perfiles',cadena]);
- }
+  locutor(cadena: string) {
+    this.router.navigate(['/perfiles', cadena]);
+  }
 
   product() {
     this.router.navigate(['/producto'])
   }
 
-  
+
 
 }
