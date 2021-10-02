efd480ede71d657435877d0aa174c8c10527d8d7
Camilo Moreno <camilomoreno@javeriana.edu.co>
2021-10-02 15:35:42 -0500

    onboarding v3

--- megavr/src/app/inicio/inicio.component.ts
+++ megavr/src/app/inicio/inicio.component.ts
@@ -23,7 +23,7 @@ export class InicioComponent implements OnInit {
   public tam: number = 0;
   public btitle: boolean = false;
   public home: boolean = false;
-  public indicador: boolean[] = [false, false, false, false, false];
+  public indicador: boolean[] = [false, false, false, false, false,false];
   public innerWidth: any;
 
   swiper !: Swiper;
@@ -59,6 +59,7 @@ export class InicioComponent implements OnInit {
   llenar() {
     this.informacion.push(new banner().createBanner("Bienvenida", "Hola te damos la bienvenida a la mega virtual"));
     this.informacion.push(new banner().createBanner("", "En tu dispositivo de escritorio presiona el clic izquierdo del ratón para navegar la experiencia y desde tu celular gira en 360° y empieza a disfrutar del contenido tocando sobre los diferentes iconos."));
+    this.informacion.push(new banner().createBanner("", "Usa tu celular de forma vertical."));
     this.informacion.push(new banner().createBanner("Sabes dónde se sienta cada dj?", "Vas a poder interactuar con cada uno de los dj's de la mega y saber las noticias del entretenimiento"));
     this.informacion.push(new banner().createBanner("Sabías que?", "La Mega se oye más del 50% de Colombia La Mega esta al aire desde 1993"));
     this.informacion.push(new banner().createBanner("Compártelo con tus amigos", ""));
