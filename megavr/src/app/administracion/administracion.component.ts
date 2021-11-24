92c61bcc0b9d9d0c277040b33f8c40515ebf12e0
Camilo Moreno <camilomoreno@javeriana.edu.co>
2021-11-24 11:55:02 -0500

    Ajuste URL administrable + redireccionamiento banner shirry

--- megavr/src/app/administracion/administracion.component.ts
+++ megavr/src/app/administracion/administracion.component.ts
@@ -9,7 +9,14 @@ export class AdministracionComponent implements OnInit {
 
   constructor() { }
 
+  public tabs: boolean[] = [true,false,false,false,false];
+
   ngOnInit(): void {
+  }
+
+  changeTab(i : any){
+    this.tabs = [false,false,false,false,false];
+    this.tabs[i] = true;
   }
 
 }
--- megavr/src/app/app-routing.module.ts
+++ megavr/src/app/app-routing.module.ts
@@ -5,10 +5,7 @@ import { PerfilesComponent } from './perfiles/perfiles.component';
 import { ProductoComponent } from './producto/producto.component';
 import { SpaceComponent } from './space/space.component';
 import { NoticiasComponent } from './noticias/noticias.component';
-import { OnboardingAdminComponent } from './administracion/onboarding-admin/onboarding-admin.component';
-import { SpaceAdminComponent } from './administracion/space-admin/space-admin.component';
-import { PerfilAdminComponent } from './administracion/perfil-admin/perfil-admin.component';
-import { NoticiaAdminComponent } from './administracion/noticia-admin/noticia-admin.component';
+import { AdministracionComponent } from './administracion/administracion.component';
 
 const routes: Routes = [
   
@@ -18,10 +15,7 @@ const routes: Routes = [
   { path: 'perfiles/:id', component: PerfilesComponent },
   { path: 'perfiles/:id_locutor/noticias/:id', component: NoticiasComponent },
   { path: 'producto', component: ProductoComponent },
-  { path: 'onboarding-admin', component: OnboardingAdminComponent },
-  { path: 'space-admin', component: SpaceAdminComponent },
-  { path: 'perfil-admin', component: PerfilAdminComponent},
-  { path: 'noticias-admin' , component: NoticiaAdminComponent}
+  { path: 'administrador' , component: AdministracionComponent}
 ];
 
 @NgModule({
